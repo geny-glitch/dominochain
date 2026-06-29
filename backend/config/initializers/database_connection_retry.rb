8dc2d886f7f2d1f833ea5adf270cd08c0cb08e1f
geny-glitch <geny@dominochain.app>
2026-06-29 20:55:26 +0200

    Fix

--- backend/config/initializers/database_connection_retry.rb
+++ backend/config/initializers/database_connection_retry.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 if Rails.env.production?
+  require Rails.root.join("lib/middleware/database_connection_retry")
+
   Rails.application.config.middleware.insert_before 0, Middleware::DatabaseConnectionRetry
 end

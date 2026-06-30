22e5740951852c3b3c5206afef3bad0fa3343d60
geny-glitch <geny@dominochain.app>
2026-06-30 17:58:19 +0200

    Better UX for restricted params

--- android/app/src/main/java/com/bg/NotificationHelper.kt
+++ android/app/src/main/java/com/bg/NotificationHelper.kt
@@ -9,7 +9,6 @@ import android.media.AudioAttributes
 import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Build
-import android.provider.Settings
 import androidx.core.app.NotificationCompat
 
 object NotificationHelper {
